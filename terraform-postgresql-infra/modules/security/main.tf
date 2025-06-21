6f72386aee51b62e65f3937188d048a43fba486e
divya <divya@pandey2510divya@gmail.com>
2025-06-21 11:53:27 +0530

    Updated

--- terraform-postgresql-infra/modules/security/main.tf
+++ terraform-postgresql-infra/modules/security/main.tf
@@ -1,13 +1,14 @@
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion-sg"
-  description = "Allow SSH"
+  description = "Allow all traffic"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.bastion_allowed_cidr]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic"
   }
 
   egress {
@@ -15,28 +16,21 @@ resource "aws_security_group" "bastion_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
 }
 
 resource "aws_security_group" "cluster_sg" {
   name        = "cluster-sg"
-  description = "Allow from bastion"
+  description = "Allow all traffic"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
-  }
-
-  # ✅ Add-on: SSH access from bastion to cluster
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
-    description     = "Allow SSH from bastion host"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic"
   }
 
   egress {
@@ -44,5 +38,6 @@ resource "aws_security_group" "cluster_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
 }
